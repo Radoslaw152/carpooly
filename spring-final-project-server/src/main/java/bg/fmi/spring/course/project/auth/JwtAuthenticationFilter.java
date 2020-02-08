f6cac0253d4dff48105aea05d2c036cd4954bd1b
ppanayotov <ppanayotov@vmware.com>
2020-02-08 13:21:52 +0200

    keep

--- spring-final-project-server/src/main/java/bg/fmi/spring/course/project/auth/JwtAuthenticationFilter.java
+++ spring-final-project-server/src/main/java/bg/fmi/spring/course/project/auth/JwtAuthenticationFilter.java
@@ -1,19 +1,6 @@
 package bg.fmi.spring.course.project.auth;
 
-import com.fasterxml.jackson.databind.JsonNode;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,13 +9,22 @@ import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 import bg.fmi.spring.course.project.constants.Constants;
 import bg.fmi.spring.course.project.constants.Role;
-import bg.fmi.spring.course.project.interfaces.services.AccountService;
 import bg.fmi.spring.course.project.utils.JsonUtil;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -40,28 +36,24 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         setFilterProcessesUrl(Constants.AUTH_LOGIN_URL);
     }
 
+    @SneakyThrows
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
-            HttpServletResponse response) throws
+                                                HttpServletResponse response) throws
             AuthenticationException {
         log.info("Attempting auth {}", request.getRequestURI());
         String email;
         String password;
         Role role;
 
-        try {
-            String body = request.getReader().lines().collect(Collectors.joining("\n"));
-            JsonNode node = JsonUtil.toJsonNode(body);
-            if (node == null) {
-                return null;
-            }
-
-            email = node.get(Constants.EMAIL_KEY).asText();
-            password = node.get(Constants.PASSWORD_KEY).asText();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
+        String body = request.getReader().lines().collect(Collectors.joining("\n"));
+        JsonNode node = JsonUtil.toJsonNode(body);
+        if (node == null) {
             return null;
         }
+
+        email = node.get(Constants.EMAIL_KEY).asText();
+        password = node.get(Constants.PASSWORD_KEY).asText();
 
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(email,
@@ -72,25 +64,25 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain, Authentication authentication) {
-        UsernamePasswordAuthenticationToken user = ((UsernamePasswordAuthenticationToken) authentication);
+                                            HttpServletResponse response,
+                                            FilterChain filterChain, Authentication authentication) {
+        UsernamePasswordAuthenticationToken user = ((UsernamePasswordAuthenticationToken)authentication);
 
         List<String> roles = user.getAuthorities()
-                .stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
+                                 .stream()
+                                 .map(GrantedAuthority::getAuthority)
+                                 .collect(Collectors.toList());
         byte[] signingKey = Constants.JWT_SECRET.getBytes();
 
         String token = Jwts.builder()
-                .signWith(Keys.hmacShaKeyFor(signingKey), SignatureAlgorithm.HS512)
-                .setHeaderParam("typ", Constants.TOKEN_TYPE)
-                .setIssuer(Constants.TOKEN_ISSUER)
-                .setAudience(Constants.TOKEN_AUDIENCE)
-                .setSubject(JsonUtil.toStringObject(user.getPrincipal()))
-                .setExpiration(new Date(System.currentTimeMillis() + 864000000))
-                .claim("role", roles)
-                .compact();
+                           .signWith(Keys.hmacShaKeyFor(signingKey), SignatureAlgorithm.HS512)
+                           .setHeaderParam("typ", Constants.TOKEN_TYPE)
+                           .setIssuer(Constants.TOKEN_ISSUER)
+                           .setAudience(Constants.TOKEN_AUDIENCE)
+                           .setSubject(JsonUtil.toStringObject(user.getPrincipal()))
+                           .setExpiration(new Date(System.currentTimeMillis() + 864000000))
+                           .claim("role", roles)
+                           .compact();
 
         response.addHeader(Constants.TOKEN_HEADER,
                 Constants.JWT_TOKEN_PREFIX + token);
